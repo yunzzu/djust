e1bb287f05c4051850faad0cd401d39b374d2b3f
yunzzu <whdsbswn@gmail.com>
2021-07-04 18:33:20 +0900

    djust Django #18 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,13 +1,19 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 from accountapp.models import HelloWorld
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get('hello_world_input')
+
         nhw = HelloWorld()  #models.py
         nhw.text = temp
         nhw.save()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': temp})
+
+        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD 🍸'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
