b055ef6c46492cffc0972ecd2af00a2f324a6288
yunzzu <whdsbswn@gmail.com>
2021-07-05 11:56:56 +0900

    djust Django #21 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccCreate
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name="hello_world")
+    path('hello_world/', hello_world, name="hello_world"),
+    path('create/', AccCreate.as_view(), name='create'),  #class형
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,10 @@
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from accountapp.models import HelloWorld
 from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 def hello_world(request):
     if request.method == "POST":
@@ -17,3 +19,10 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccCreate(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:helloworld')  # 계정 만드는 것에 성공했다면 어느 경로로 다시 연결해야할지
+    #reverse(함수에서 사용) vs reverse_lazy(클래스에서 사용)
+    template_name = 'accountapp/create.html' # 회원가입을 할 때 어느 html파일을 이용해서 볼지
